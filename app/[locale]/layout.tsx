9752a5faeb99cfa76b0cb543c1a36a5126ece90a
consennse <urs.senn@consennse.com>
2025-01-08 13:26:16 +0530

    favicon2

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import favicon from "@/public/favicon.ico"
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -24,6 +24,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+        {/* ⁠ <head>
+          <link rel='icon' href='/favicon.ico' sizes='any' />
+          </head> ⁠ */}
+
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
