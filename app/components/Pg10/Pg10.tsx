efd088c58dfe1cb4c833c721d667ba7b02529026
consennse <urs.senn@consennse.com>
2025-01-28 17:29:06 +0530

    German AboutUS

--- app/components/Pg10/Pg10.tsx
+++ app/components/Pg10/Pg10.tsx
@@ -3,7 +3,8 @@ import React from 'react'
 import style from "./Pg10.module.css"
 import Nav from "@/app/components/Nav/Nav"
 import Footer from '../Footer/Footer'
-// import { useTranslation } from 'react-i18next'
+import { useTranslation } from 'react-i18next'
+import Link from 'next/link';
 import pfp from "./image-7.svg"
 import pfp2 from "./image-8.svg"
 import pfp3 from "./image-9.svg"
@@ -23,70 +24,86 @@ import logo10 from "./NAR - Logo 1.svg"
 import logo11 from "./Nova Casa 1.svg"
 import logo12 from "./logo-website-swiss-homes 1.svg"
 const Pg10 = () => {
-  // const {t} = useTranslation();
+  const {t} = useTranslation();
   return (
     <div className={style.body}>
         <Nav/>
         <div className={style.main}>
           <div className = {style.top}>
-            <div className = {style.heading1}>Meet the Team</div>
+            <div className = {style.heading1}>{t('500')}</div>
             <div className = {style.line}></div>
-            <div className = {style.para}>We’re a dedicated core team, working closely together to ensure outstanding results. To bring even more expertise and flexibility to our projects, we also collaborate with a network of talented freelancers. Together, we deliver the best solutions for every challenge!</div>
+            <div className = {style.para}>{t('501')}</div>
             <div className = {style.box}>
               <div className = {style.box1}>
                 <div className = {style.img1}> <Image src = {pfp} alt ="image" className={style.logo10}/>  </div>
                 <div className = {style.head1}>Urs Senn</div>
-                <div className={style.bod1}>The greatest joy as Managing Director and Founder is building something sustainable and valuable together with a strong team.</div>
-                <div className = {style.maincon1}>Managing Director / Founder</div>
-                <div className = {style.icon}> <Image src = {mail} alt = "email" className={style.mail} /> </div>
+                <div className={style.bod1}>{t('502')}</div>
+                <div className = {style.maincon1}>{t('503')}</div>
+                <div className = {style.icon}>
+                  <Link href="mailto:urs.senn@consennse.com" target="_blank" rel="noopener noreferrer">
+                   <Image src = {mail} alt = "email" className={style.mail} /> 
+                   </Link>
+                   </div>
               </div>
         
               <div className = {style.box1}>
                 <div className = {style.img1}> <Image src = {pfp2} alt ="image" className={style.logo10}/>  </div>
                 <div className = {style.head1}>Anna Hormann</div>
-                <div className={style.bod1}>I love keeping an overview in accounting and administration while making an important contribution to the team’s success.</div>
-                <div className = {style.maincon1}>Accounting / Backoffice</div>
-                <div className = {style.icon}> <Image src = {mail} alt = "email" className={style.mail}/></div>
+                <div className={style.bod1}>{t('504')}</div>
+                <div className = {style.maincon1}>{t('505')}</div>
+                <div className = {style.icon}>
+                <Link href="mailto:servicedesk@consennse.com" target="_blank" rel="noopener noreferrer">
+                   <Image src = {mail} alt = "email" className={style.mail}/>
+                   </Link>
+                   </div>
               </div>
             
               <div className = {style.box1}>
                 <div className = {style.img1}> <Image src = {pfp3} alt ="image" className={style.logo10}/>  </div>
                 <div className = {style.head1}>Balazs Rubicsek</div>
-                <div className={style.bod1}>As a Senior Technical Consultant, I love solving complex challenges and developing innovative solutions for our clients.</div>
-                <div className = {style.maincon1}>Senior Technical Consultant</div>
-                <div className = {style.icon}> <Image src = {mail} alt = "email" className={style.mail}/></div>
+                <div className={style.bod1}>{t('506')}</div>
+                <div className = {style.maincon1}>{t('507')}</div>
+                <div className = {style.icon}>
+                <Link href="mailto:balazs.rubicsek@consennse.com" target="_blank" rel="noopener noreferrer">
+                   <Image src = {mail} alt = "email" className={style.mail}/>
+                   </Link>
+                   </div>
               </div>
          
               <div className = {style.box1}>
                 <div className = {style.img1}> <Image src = {pfp4} alt ="image" className={style.logo10}/>  </div>
                 <div className = {style.head1}>Sarah Spandolf</div>
-                <div className={style.bod1}>As a Propertybase Administrator, I ensure that our customers’ systems run smoothly and provide the best possible value to their users.</div>
-                <div className = {style.maincon1}>Propertybase Administrator</div>
-                <div className = {style.icon}> <Image src = {mail} alt = "email" className={style.mail}/></div>
+                <div className={style.bod1}>{t('508')}</div>
+                <div className = {style.maincon1}>{t('509')}</div>
+                <div className = {style.icon}> 
+                <Link href="mailto:sarah.spandolf@consennse.com" target="_blank" rel="noopener noreferrer">
+                <Image src = {mail} alt = "email" className={style.mail}/>
+                </Link>
+                </div>
               </div>
             </div>
           </div>
           <div className = {style.bottom}>
-            <div className = {style.heading1}>We deliver project successes </div>
+            <div className = {style.heading1}>{t('510')}</div>
             <div className = {style.line}></div>
-            <div className = {style.para}>Our organization has achieved numerous notable project successes in recent years, which underline our competence and commitment. </div>
+            <div className = {style.para}>{t('511')}</div>
             <div className = {style.boxes2}>
             <div className= {style.box22}>
             <div className = {style.box2}>
             <div className = {style.heading2}>Andermatt Swiss Alps AG</div>
             <div className = {style.logo}><Image src = {logo1} alt = "logo" className={style.logo20}/> </div>
             <div className = {style.line2}></div>
-            <div className = {style.bod2}>Our Services and Solutions</div>
+            <div className = {style.bod2}>{t('512')}</div>
             <div className = {style.pointers}>
               <ul className = {style.list}>
-                <li>Implementation of Propertybase (replacement of Flowfact, data migration) </li>
-                <li>Rental Solution (management of rental contracts from owners)</li>
-                <li>Sales Partner Portal (lead registration for partners)</li>
-                <li>Sales Target Solution (including commission calculation)</li>
-                <li>Website Integration (company website, micro-sites for properties)</li>
-                <li>Social Media Integration (Meta, LinkedIn)</li>
-                <li>UTM Integration for lead source tracking (all channels)</li>
-                <li>Mailchimp Integration with double opt-in and synchronization with the CRM</li>
+                <li>{t('513')}</li>
+                <li>{t('514')}</li>
+                <li>{t('515')}</li>
+                <li>{t('516')}</li>
+                <li>{t('517')}</li>
+                <li>{t('518')}</li>
+                <li>{t('519')}</li>
+                <li>{t('520')}</li>
               </ul>
             </div>
             </div>
@@ -94,15 +111,15 @@ const Pg10 = () => {
             <div className = {style.heading2}>INIZIA Living AG</div>
             <div className = {style.logo}><Image src = {logo2} alt = "logo" className={style.logo20}/> </div>
             <div className = {style.line2}></div>
-            <div className = {style.bod2}>Our Services and Solutions</div>
+            <div className = {style.bod2}>{t('512')}</div>
             <div className = {style.pointers}>
               <ul className = {style.list}>
-              <li>Implementation of Propertybase</li>
-<li>Website Integration (company website, micro-sites for projects)</li>
-<li>Automated dispatch and follow-up process</li>
-<li>Buyer support (from contract signing to move-in)</li>
-<li>Marketing through external partners</li>
-<li>Client reporting with Power Query
+              <li>{t('522')}</li>
+<li>{t('523')}</li>
+<li>{t('524')}</li>
+<li>{t('525')}</li>
+<li>{t('526')}</li>
+<li>{t('527')}
 </li>
 </ul>
             </div>
@@ -113,17 +130,17 @@ const Pg10 = () => {
             <div className = {style.heading2}>Markstein AG</div>
             <div className = {style.logo}><Image src = {logo3} alt = "logo" className={style.logo30}/> </div>
             <div className = {style.line2}></div>
-            <div className = {style.bod2}>Our Services and Solutions</div>
+            <div className = {style.bod2}>{t('512')}</div>
             <div className = {style.pointers}>
               <ul className = {style.list}>
-           <li>   Implementation of Propertybase (replacement of Flowfact, data migration) </li>
-<li>Website Integration (company website, micro-sites for projects) </li>
-<li>Automated dispatch and follow-up process </li>
-<li>Budget control for advertisements on portals </li>
-<li>Client reporting </li>
-<li>Marketing activities with Power Query </li>
-<li>Email campaigns with follow-up </li>
-<li>Lead capturing from Neubau Portal </li>
+           <li>{t('528')}</li>
+<li>{t('529')}</li>
+<li>{t('530')}</li>
+<li>{t('531')}</li>
+<li>{t('532')}</li>
+<li>{t('533')}</li>
+<li>{t('534')}</li>
+<li>{t('535')}</li>
               </ul>
             </div>
             </div>            
@@ -131,16 +148,16 @@ const Pg10 = () => {
             <div className = {style.heading2}> WALDE Immobilien AG</div>
             <div className = {style.logo}><Image src = {logo4} alt = "logo" className={style.logo30}/> </div>
             <div className = {style.line2}></div>
-            <div className = {style.bod2}>Our Services and Solutions</div>
+            <div className = {style.bod2}>{t('512')}</div>
             <div className = {style.pointers}>
               <ul className = {style.list}>
-            <li>  Implementation of Propertybase </li>
-<li>Survey solution with synchronization with the CRM </li>
-<li>Website integration (company website, micro-sites for projects) </li>
-<li>Automated dispatch and follow-up process </li>
-<li>Campaign Monitor integration with synchronization with the CRM </li>
-<li>Integration with Microsoft Power BI </li>
-<li>Automated exchange of listings with partner agents </li>
+            <li> {t('536')} </li>
+<li>{t('537')}</li>
+<li>{t('538')}</li>
+<li>{t('539')}</li>
+<li>{t('540')}</li>
+<li>{t('541')}</li>
+<li>{t('542')}</li>
               </ul>
             </div>
             </div>
@@ -148,7 +165,7 @@ const Pg10 = () => {
             </div>
           </div>
           <div className={style.companies}>
-          <div className = {style.heading3}>Further companies that rely on us</div>
+          <div className = {style.heading3}>{t('543')}</div>
           <div className = {style.line}></div>
           <div className = {style.logos}>
             <div className = {style.row}>
