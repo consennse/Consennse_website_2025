acf92d15d87a9cc65933e0748c523bff86b13af0
consennse <urs.senn@consennse.com>
2025-03-17 22:29:17 +0530

    start

--- app/[locale]/content/support/page.tsx
+++ app/[locale]/content/support/page.tsx
@@ -5,6 +5,27 @@ import Pg4 from '@/app/components/Pg4/Pg4';
 import initTranslations from '@/app/i18n';
 import TranslationsProvider from '@/app/components/TranslationProvider';
 
+
+
+export async function generateMetadata({ params }: { params: Promise<{ locale?: string }> }) {
+
+  const locale = (await params)?.locale || 'en'; // Default to English
+  const metaDataEN = {
+    title: "Propertybase CRM Services | Consulting, Implementation & Support",
+    description: "We guide you from consultation to implementation and ongoing support for Propertybase CRM. Optimize your real estate business with tailored solutions, seamless integrations, and expert support.",
+  };
+
+  const metaDataDE = {
+    title: "Propertybase CRM Support | Flexible Service-Modelle & schnelle Hilfe",
+    description: "Individueller Propertybase CRM-Support: Wählen Sie das passende Service-Level mit garantierten Reaktionszeiten und massgeschneiderter Betreuung. Unser Expertenteam unterstützt Sie zuverlässig und effizient.",
+  };
+
+  const selectedMetadata = locale === 'de' ? metaDataDE : metaDataEN;
+  console.log("🔍 Debug: Selected Metadata:", selectedMetadata);
+
+  return selectedMetadata;
+}
+
 export async function generateStaticParams() {
   return [
     { locale: 'en' },
